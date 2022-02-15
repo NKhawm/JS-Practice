f49c37bd6b1ac70c256833b8e7b2692d91729754
Niang Khawm Huai <khawmhuai@gmail.com>
2022-02-15 15:30:16 +0000

    magicshow

--- magic.js
+++ magic.js
@@ -1,5 +1,23 @@
-function price (){
-    let yName = document.getElementById('name').value;
-    let yEmail = document.getElementById('email').value;
-    
+function showPrice (){
+    var age = document.getElementById('ageGroup').value;
+
+    if (age==10 || age < 10)
+    {
+        alert('£5')
+
+    }
+    else if (age>10 && age<18)
+    {
+        alert("£8")
+
+    }
+    else if (age > 17 && age<65)
+    {
+        alert('£12')
+    }
+    else if (age==65 || age > 65)
+    {
+        alert('£8')
+    }
+   
 }
