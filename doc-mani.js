3b09ab6aa586e82c0d6ae7f04c76ebb11c9876cf
Niang Khawm Huai <khawmhuai@gmail.com>
2022-03-02 16:56:05 +0000

    new

---
+++ doc-mani.js
@@ -0,0 +1,8 @@
+var n =document.getElementById('heading').innerHTML;
+//document.write(n)
+//replacing the content
+document.getElementById('heading').innerHTML="Document Object Model"
+document.getElementById('list-heading').innerText='Hello'
+document.getElementById('list-heading').textContent='Hello,see the content'
+document.getElementById('heading').style.color = "#ff0000";
+document.getElementById('heading').style.backgroundColor ='yellow';
