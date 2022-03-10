80369850ae06dde357e9ce3b1fe6ef8eb1d98319
Niang Khawm Huai <khawmhuai@gmail.com>
2022-03-10 22:05:19 +0000

    up

--- jsdropdown.js
+++ jsdropdown.js
@@ -23,3 +23,20 @@ function myFunction(){
       z.innerHTML = 'New Cell2';
 
   }
+
+  //accept row and column and update string
+
+  function createTbl(){
+      row = prompt('Input number of rows' ,1);
+      column = prompt ('Input number of columns',1);
+      
+      for (var r=0; r<parseInt (row,10);r++)
+      {
+          var x = document.getElementById('tble').insertRow(r);
+          for (var c=0; c<parseInt(column,10);c++)
+          {
+              var y= x.insertCell(c);
+              y.innerHTML="Row" +r+"Column"+c;
+          }
+      }
+  }
